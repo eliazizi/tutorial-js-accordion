5984e61191312b573ba0363f744e30b496a83d48
Elham Azizi <elhamazizi09@gmail.com>
2019-05-10 00:34:00 +0430

    add classes using JS

--- src/js/jsaccordion.js
+++ src/js/jsaccordion.js
@@ -5,6 +5,28 @@ function JSAccordion(elementOrSelector) {
     //  define public methods
 
     this.init=function () {
+        this.targetElement.classList.add('container');
+        var ul=this.targetElement.querySelector("ul");
+        ul.classList.add('list');
+        var all_li=ul.querySelectorAll('li');
+        all_li.forEach(function (element) {
+            element.classList.add('list-item');
+            var first_div=element.querySelector("div:first-child");
+            first_div.classList.add('header');
+            var span=first_div.querySelector('span');
+            span.classList.add('status-icon');
+            var h=first_div.querySelector('h3');
+            h.classList.add('titl_h');
+            var second_div=element.querySelector("div:last-child");
+            second_div.classList.add('body');
+            var all_p=second_div.querySelectorAll('p')
+            all_p.forEach(function (element_two) {
+                element_two.classList.add('p_body');
+
+            })
+        });
+
+        
 
     }
 
